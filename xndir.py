3795cc8d4582f1787ccfcb768f1727569dbee43c
MuradGhazaryan <muradghazaryan8@gmail.com>
2024-05-10 05:02:45 -0800

    init

---
+++ xndir.py
@@ -0,0 +1,20 @@
+def filter(arr, bajanord): #veradarcnel bolor zuyq elementnere voronq bajanvumen bajanordi vrya
+    return [10, 20]
+
+def sort(arr): #veradarcnel sortavorac list
+    return arr
+
+def isValid(arr, expectedStr): #stugel ardzyok trvac tveri liste hamapatasxanume aknkalvox patasxanin
+    return False
+
+a = [2, 3, 10, 7, 6, 15, 20]
+
+filterRes = filter(a, 5)
+if isValid(filterRes, '10,20') == False:
+    print('Filtravorume sxale')
+
+sortRes = sort(filterRes)
+if isValid(sortRes, '10,20') == False:
+    print('Sortavorume sxale')
+
+
