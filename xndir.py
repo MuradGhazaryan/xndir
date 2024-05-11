25fab44299797f6230dbbc43d9998b502f691183
MuradGhazaryan <muradghazaryan8@gmail.com>
2024-05-11 04:44:29 -0800

    solution

--- xndir.py
+++ xndir.py
@@ -2,12 +2,22 @@ def filter(arr, bajanord): #veradarcnel bolor zuyq elementnere voronq bajanvumen
     return [10, 20]
 
 def sort(arr): #veradarcnel sortavorac list - Murad
+    arr = a
+    for num in range(len(arr)-1, 0, -1):
+        for i in range(num):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
     return arr
+
+
 
 def isValid(arr, expectedStr): #stugel ardzyok trvac tveri liste hamapatasxanume aknkalvox patasxanin - Artur
     return False
 
 a = [2, 3, 10, 7, 6, 15, 20]
+arr = a
+rezult = sort(arr)
+print(rezult)  
 
 filterRes = filter(a, 5)
 if isValid(filterRes, '10,20') == False:
