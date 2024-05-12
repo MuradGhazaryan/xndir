a805da594594e5273b9624f20ab320f1f71a1280
ll9llall9ll <babakhanyana74@gmail.com>
2024-05-12 15:02:00 +0400

    implemented filter isvalid with tests

--- xndir.py
+++ xndir.py
@@ -1,11 +1,52 @@
-def filter(arr, bajanord): #veradarcnel bolor zuyq elementnere voronq bajanvumen bajanordi vrya - Anahit
-    return [10, 20]
+def filter(arr: list[int], divisor: int) -> list[int]:
+    """veradarcnel bolor zuyq elementnere voronq bajanvumen bajanordi vrya - Anahit
+
+    Args:
+        arr (list[int]): tvain list vore petqe filtrvi
+        divisor (int): bajanord vore ogtagorcveluya filterlu hamar
+
+    Returns:
+        list[int]: filtrac list
+    """
+    res = []
+    
+    for num in arr:
+        num_int = int(num)  
+        if num_int % 2 == 0 and num_int % divisor == 0:  
+            res.append(num_int)
+
+    return res
 
 def sort(arr): #veradarcnel sortavorac list - Murad
     return arr
 
 def isValid(arr, expectedStr): #stugel ardzyok trvac tveri liste hamapatasxanume aknkalvox patasxanin - Artur
-    return False
+    expectedArr = expectedStr.split(',')
+    for i in range(len(arr)):
+        if int(arr[i]) != int(expectedArr[i]):
+            return False
+    
+    return True
+
+# print('Testing IsValid with arr = [1,2,3], expectedStr = 1,2,3...')
+# if isValid([1,2,3], '1,2,3'):
+#     print('Test passed!')
+# else:
+#     print('Test failed!')
+
+# print('Testing IsValid with arr = [2,4,6], expectedStr = 1,2,3...')
+# if isValid([2,4,6], '1,2,3'):
+#     print('Test failed!')
+# else:
+#     print('Test passed!')
+
+# print('Testing IsValid with arr = [2,3,4], expectedStr = 2,3,4...')
+# if isValid([2,3,4], '2,3,4'):
+#     print('Test passed!')
+# else:
+#     print('Test failed!')
+
+
 
 a = [2, 3, 10, 7, 6, 15, 20]
 
